147a7596106528665038ee097b4d072beff96c73
the-best-is-best <michelle.raouf@outlook.com>
2022-07-30 14:55:33 +0200

    make Pages for tab bar

---
+++ lib/presentation/home/pages/all_tasks.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AllTasks extends StatelessWidget {
+  const AllTasks({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("All"),
+    );
+  }
+}
--- lib/presentation/home/view/home_view.dart
+++ lib/presentation/home/view/home_view.dart
@@ -1,28 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:m_todo_app_tutorial/presentation/home/pages/all_tasks.dart';
+import 'package:m_todo_app_tutorial/presentation/home/pages/completed_tasks.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
+    return const DefaultTabController(
       length: 4,
       child: Scaffold(
-        appBar: AppBar(
-          title: const Text("Home View"),
-          actions: [
-            IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
-            IconButton(
-                onPressed: () {}, icon: const Icon(Icons.calendar_today)),
-            IconButton(onPressed: () {}, icon: const Icon(Icons.language)),
-          ],
-          bottom: const TabBar(isScrollable: true, tabs: [
-            Tab(text: 'All'),
-            Tab(text: 'Completed'),
-            Tab(text: 'UnCompleted'),
-            Tab(text: 'Favorite'),
-          ]),
-        ),
+        appBar: PreferredSize(
+            preferredSize: Size.fromHeight(100.0), child: MyAppBar()),
+        body: TabBarView(children: [
+          AllTasks(),
+          CompletedTasks(),
+          AllTasks(),
+          CompletedTasks()
+        ]),
+      ),
+    );
+  }
+}
+
+class MyAppBar extends StatelessWidget {
+  const MyAppBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: const Text("Home View"),
+      actions: [
+        IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+        IconButton(onPressed: () {}, icon: const Icon(Icons.calendar_today)),
+        IconButton(onPressed: () {}, icon: const Icon(Icons.language)),
+      ],
+      bottom: const TabBar(
+        indicatorColor: Colors.tealAccent,
+        isScrollable: true,
+        tabs: [
+          Tab(text: 'All'),
+          Tab(text: 'Completed'),
+          Tab(text: 'UnCompleted'),
+          Tab(text: 'Favorite'),
+        ],
       ),
     );
   }
