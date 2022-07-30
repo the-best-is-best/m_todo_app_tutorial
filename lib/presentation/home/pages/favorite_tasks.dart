1d5603133467cf173a4c111fd4b90310b2c7baf3
the-best-is-best <michelle.raouf@outlook.com>
2022-07-30 20:50:26 +0200

    Tab Bar

---
+++ lib/presentation/home/pages/favorite_tasks.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FavoriteTasks extends StatelessWidget {
+  const FavoriteTasks({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("4"),
+    );
+  }
+}
--- lib/presentation/home/view/home_view.dart
+++ lib/presentation/home/view/home_view.dart
@@ -1,28 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:m_todo_app_tutorial/presentation/home/pages/all_tasks.dart';
+import 'package:m_todo_app_tutorial/presentation/home/pages/completed_tasks.dart';
+import 'package:m_todo_app_tutorial/presentation/home/pages/favorite_tasks.dart';
+import 'package:m_todo_app_tutorial/presentation/home/pages/un_completed_tasks.dart';
+
+import '../widgets/my_app_bar.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
+    return const DefaultTabController(
       length: 4,
       child: Scaffold(
-        appBar: AppBar(
-          title: const Text("Home View"),
-          actions: [
-            IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
-            IconButton(
-                onPressed: () {}, icon: const Icon(Icons.calendar_today)),
-            IconButton(onPressed: () {}, icon: const Icon(Icons.language)),
-          ],
-          bottom: const TabBar(isScrollable: true, tabs: [
-            Tab(text: 'All'),
-            Tab(text: 'Completed'),
-            Tab(text: 'UnCompleted'),
-            Tab(text: 'Favorite'),
-          ]),
-        ),
+        appBar: PreferredSize(
+            preferredSize: Size.fromHeight(100), child: MyAppBar()),
+        body: TabBarView(children: [
+          AllTasks(),
+          CompletedTasks(),
+          UnCompletedTasks(),
+          FavoriteTasks(),
+        ]),
       ),
     );
   }
