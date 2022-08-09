dc4c61f74ec2be8769cbd3b3eac787e7aef5c06a
the-best-is-best <michelle.raouf@outlook.com>
2022-08-09 23:32:36 +0200

    merge

--- lib/app/components/my_text.dart
+++ lib/app/components/my_text.dart
@@ -4,18 +4,6 @@ class MyText extends StatelessWidget {
   const MyText({
     Key? key,
     required this.title,
-<<<<<<< HEAD
-    this.style,
-  }) : super(key: key);
-  final String title;
-  final TextStyle? style;
-  @override
-  Widget build(BuildContext context) {
-    //  return Text("Add A Task");
-    return Text(
-      title,
-      style: style,
-=======
     this.textStyle,
   }) : super(key: key);
   final String title;
@@ -26,7 +14,6 @@ class MyText extends StatelessWidget {
     return Text(
       title,
       style: textStyle,
->>>>>>> episode-5-add-a-task-ui
     );
   }
 }
