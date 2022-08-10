d21bb538fa5b3c37248e1131fa0afdedbb723aac
the-best-is-best <michelle.raouf@outlook.com>
2022-08-10 18:29:53 +0200

    merge

--- lib/presentation/add_a_task/view/add_a_task_view.dart
+++ lib/presentation/add_a_task/view/add_a_task_view.dart
@@ -19,18 +19,15 @@ class _AddATaskViewState extends State<AddATaskView> {
     dateController = TextEditingController();
     startTimeController = TextEditingController();
     endTimeController = TextEditingController();
-<<<<<<< HEAD
     dateController.text = DateFormat("yyyy-MM-dd").format(DateTime.now());
     startTimeController.text = DateFormat("hh:mm a").format(DateTime.now());
     endTimeController.text = DateFormat("hh:mm a").format(DateTime.now());
-=======
 
     dateController.text = DateFormat("yyyy-MM-dd").format(DateTime.now());
     startTimeController.text = DateFormat("hh:mm a").format(DateTime.now());
     endTimeController.text = DateFormat("hh:mm a")
         .format(DateTime.now().add(const Duration(minutes: 10)));
 
->>>>>>> episode-6-select-time-date
     super.initState();
   }
 
@@ -68,7 +65,6 @@ class _AddATaskViewState extends State<AddATaskView> {
                         context: context,
                         initialDate: DateTime.now(),
                         firstDate: DateTime.now(),
-
                         lastDate: DateTime(
                           DateTime.now().year + 10,
                         ));
@@ -99,15 +95,16 @@ class _AddATaskViewState extends State<AddATaskView> {
                           if (timeSelected != null) {
                             startTimeController.text =
                                 timeSelected.format(context);
-                          TimeOfDay? selectTime = await showTimePicker(
-                              context: context, initialTime: TimeOfDay.now());
-                          if (selectTime != null) {
-                            if (!mounted) {
-                              return;
+                            TimeOfDay? selectTime = await showTimePicker(
+                                context: context, initialTime: TimeOfDay.now());
+                            if (selectTime != null) {
+                              if (!mounted) {
+                                return;
+                              }
+                              startTimeController.text =
+                                  selectTime.format(context);
                             }
-                            startTimeController.text =
-                                selectTime.format(context);
-                          }}
+                          }
                         },
                         onSaved: (newValue) {},
                       ),
@@ -128,14 +125,16 @@ class _AddATaskViewState extends State<AddATaskView> {
                           if (timeSelected != null) {
                             endTimeController.text =
                                 timeSelected.format(context);
-                          TimeOfDay? selectTime = await showTimePicker(
-                              context: context, initialTime: TimeOfDay.now());
-                          if (selectTime != null) {
-                            if (!mounted) {
-                              return;
+                            TimeOfDay? selectTime = await showTimePicker(
+                                context: context, initialTime: TimeOfDay.now());
+                            if (selectTime != null) {
+                              if (!mounted) {
+                                return;
+                              }
+                              endTimeController.text =
+                                  selectTime.format(context);
                             }
-                            endTimeController.text = selectTime.format(context);
-                                                    }                          }
+                          }
                         },
                         onSaved: (newValue) {},
                       ),
